048fc23522c7e9cb13bf72dc40b059b1195f4d3c
Kirill Gubin <kirill2000gubin@mail.ru>
2023-06-29 01:27:36 +0300

    Добавьте файлы проекта.

---
+++ Models/MainModels.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MeasurTest.Model
+{
+    public class Measurement : INotifyPropertyChanged
+    {
+        private string number;
+        private string fullName;
+        private string address;
+        private string phoneNumber;
+        private DateTime? date;
+
+        public string idNumber
+        {
+            get { return number; }
+            set { SetProperty(ref number, value); }
+        }
+        public string FullName
+        {
+            get { return fullName; }
+            set { SetProperty(ref fullName, value); }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { SetProperty(ref address, value); }
+        }
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { SetProperty(ref phoneNumber, value); }
+        }
+        public DateTime? Date
+        {
+            get { return date; }
+            set { SetProperty(ref date, value); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                OnPropertyChanged(propertyName);
+            }
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class MeasurementModel
+    {
+        public List<Measurement> GenerateRandomMeasurements(int count)
+        {
+            List<Measurement> measurements = new List<Measurement>();
+            Random random = new Random();
+
+            string[] cities = { "Москва", "Ростов", "Краснодар", "Челябинск", "Санкт-Петербург" };
+
+            for (int i = 1; i <= count; i++)
+            {
+                string number = $"{i}";
+                string fullName = $"Client{i}";
+                string address = $"Address{i} " + cities[random.Next(cities.Length)];
+                string phoneNumber = $"Phone{i}";
+
+
+                Measurement measurement = new Measurement
+                {
+                    idNumber = number,
+                    FullName = fullName,
+                    Address = address,
+                    PhoneNumber = phoneNumber
+                };
+
+                measurements.Add(measurement);
+            }
+
+            return measurements;
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+using MeasurTest.Model;
+
+namespace MeasurTest.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            MeasurementModel measurementModel = new MeasurementModel();
+            Measurements = measurementModel.GenerateRandomMeasurements(100);
+        }
+
+        private Measurement measurement;
+        public string idNumber
+        {
+            get { return measurement.idNumber; }
+            set
+            {
+                if (measurement.idNumber != value)
+                {
+                    measurement.idNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string FullName
+        {
+            get { return measurement.FullName; }
+            set
+            {
+                if (measurement.FullName != value)
+                {
+                    measurement.FullName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string Address
+        {
+            get { return measurement.Address; }
+            set
+            {
+                if (measurement.Address != value)
+                {
+                    measurement.Address = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string PhoneNumber
+        {
+            get { return measurement.PhoneNumber; }
+            set
+            {
+                if (measurement.PhoneNumber != value)
+                {
+                    measurement.PhoneNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public DateTime? Date
+        {
+            get { return measurement.Date; }
+            set
+            {
+                if (measurement.Date != value)
+                {
+                    measurement.Date = value;
+                    OnPropertyChanged();
+                    UpdateAvailableMeasurementsCount();
+                }
+            }
+        }
+        public List<Measurement> Measurements { get; set; }
+
+        private List<Measurement> filteredList;
+        public List<Measurement> FilteredList
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(SelectedCity) || SelectedCity.Contains("город"))
+                    return Measurements;
+
+                return filteredList;
+            }
+            set
+            {
+                filteredList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private DateTime? selectedDate;
+        public DateTime? SelectedDate
+        {
+            get { return selectedDate; }
+            set
+            {
+                if (selectedDate != value)
+                {
+                    selectedDate = value;
+                    OnPropertyChanged();
+                    UpdateAvailableMeasurementsCount();
+                }
+            }
+        }
+
+        private string availableMeasurementsCount;
+        public string AvailableMeasurementsCount
+        {
+            get { return availableMeasurementsCount; }
+            set
+            {
+                if (availableMeasurementsCount != value)
+                {
+                    availableMeasurementsCount = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private void UpdateAvailableMeasurementsCount()
+        {
+            if (!string.IsNullOrEmpty(SelectedCity)&& SelectedCity != "Выберите город")
+            {
+                Random random = new Random();
+                AvailableMeasurementsCount = SelectedDate + " в городе " + SelectedCity + " доступно замеров: " + random.Next(1, 10).ToString();
+            }
+            else
+            {
+                AvailableMeasurementsCount = string.Empty;
+            }
+        }
+
+        private string selectedCity;
+        public string SelectedCity
+        {
+            get { return selectedCity; }
+            set
+            {
+                if (selectedCity != value )
+                {
+                    selectedCity = "Запланированные замеры в городе: " + value;
+                    OnPropertyChanged();
+                    selectedCity = value;
+                    FilteredList = Measurements.Where(m => m.Address.Contains(selectedCity)).ToList();
+                    UpdateAvailableMeasurementsCount();
+                }
+                else
+                {
+                    selectedCity = "Запланированные замеры во всех городах";
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
